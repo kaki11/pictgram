2fbe86541b153114f6f8e9b615be3c563d7e0748
kakizakimaki <xxxmaki112@gmail.com>
2019-09-07 20:07:51 +0900

    add Topic model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, format: { with:/\A\S+@\S+\.\S+\z/ }
-  validates :password, length: { in: 8..32 }, format: { with:/\A[a-zA-Z0-9]+\z/ }
+  validates :password, length: { in: 8..32 }, format: { with:/(?=.*?[a-zA-Z])(?=.*?\d)[\w]/ }
 
   has_secure_password
+  has_many :topics
 end
