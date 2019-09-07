13dbd2b46c24059397b5297005dc0a1dbfecbfd7
kakizakimaki <xxxmaki112@gmail.com>
2019-09-07 11:24:38 +0900

    add helper_method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(session_params)
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
@@ -19,6 +19,10 @@ class SessionsController < ApplicationController
   end
 
   private
+  def session_params
+    params.require(:session).permit(:name, :email)
+  end
+
   def log_in(user)
     session[:user_id] = user.id
   end
