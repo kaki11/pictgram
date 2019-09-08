83389a88d7d5f79783f088806ceb785542b872d2
kakizakimaki <xxxmaki112@gmail.com>
2019-09-08 14:02:19 +0900

    RMagick

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(session_params)
+    user = User.find_by(email: session_params[:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
 
   private
   def session_params
-    params.require(:session).permit(:name, :email)
+    params.require(:session).permit(:email, :password)
   end
 
   def log_in(user)
