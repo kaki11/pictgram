89e5ca69799ea889c64f3b6fafc37d003e80209d
kakizakimaki <xxxmaki112@gmail.com>
2019-09-09 14:44:02 +0900

    SessionsControllerの修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: session_params[:email])
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(session_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
