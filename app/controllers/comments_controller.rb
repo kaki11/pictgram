1a737e4752ce4f0c270d484602206da6afcc3b7f
kakizakimaki <xxxmaki112@gmail.com>
2019-09-10 00:12:27 +0900

    CommentControllerの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,6 @@ class CommentsController < ApplicationController
 
   def create
     @comment = current_user.comments.new(comment_params)
-    @comment.topic_id = comment_params[:topic_id]
     if @comment.save
       redirect_to topics_path, success: 'コメントに成功しました'
     else
